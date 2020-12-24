440a3f58a084db3f7fd2b654c37002467ac5ea79
zhoufuming <18219116517@163.com>
2020-12-24 20:03:33 +0800

    first submit

---
+++ src/module/index/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./images/logo.png">
+    <hello></hello>
+    <a href="../module/detail.html">跳转到详情页</a>
+  </div>
+</template>
+
+<script>
+  import 'common/css/reset.css';
+  import Hello from 'components/Hello/Hello'
+
+  export default {
+    name: 'app',
+    components: {
+      Hello
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
